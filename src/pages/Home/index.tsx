94161de9e990e11628cd53ef17698842a8f5afce
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 18:44:38 -0300

    feat(Home): implement components and styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import * as Style from './style'
 
 const Home = () => {
+  const navigate = useNavigate()
+
   return (
     <Style.MainPage>
-      Home
+      <Style.CardPage>
+        <Style.TitlePage>Tech Blog</Style.TitlePage>
+
+        <Style.Description>Um blog exclusivo para <span>Desenvolvedores</span>, <span>Designers</span>, <span>Programadores</span> e amantes da tecnologia!</Style.Description>
+
+        <Style.Description>Faça o login para entrar!</Style.Description>
+
+        <Style.ButtonLog onClick={() => navigate('/login')}>Fazer login</Style.ButtonLog>
+      </Style.CardPage>
     </Style.MainPage>
   )
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,10 +1,69 @@
 import styled, { css } from "styled-components";
 
 export const MainPage = styled.main`
-    ${({theme}) => css`
-        background-color: ${theme.colors.primaryColor};
-        min-width: 100vw;
-        min-height: 100vh;
-        text-align: center;
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primaryColor};
+    min-width: 100vw;
+    min-height: 100vh;
+    text-align: center;
+    ${theme.mixins.bodyStyle}
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const CardPage = styled.section`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: ${theme.colors.cards};
+    border-radius: 15px;
+    width: 50vw;
+    height: 45vh;
+  `}
+`;
+
+export const TitlePage = styled.h1`
+  ${({ theme }) => css`
+    ${theme.mixins.titleStyle}
+    text-align: center;
+    margin: 1rem 0;
+  `}
+`;
+
+export const Description = styled.p`
+    ${() => css`
+        width: calc(100% - 60%);
+        margin: .6rem 0;
+        color: #f4f5f9;
+        span {
+            color: #000;
+            font-style: italic;
+        }
     `}
 `
+
+export const ButtonLog = styled.button`
+  ${({ theme }) => css`
+    width: calc(100% - 50%);
+    height: 45px;
+    border: transparent;
+    border-radius: 10px;
+    background-color: ${theme.colors.secondaryColor};
+    color: ${theme.colors.bodyColor};
+    box-shadow: 4px 4px 10px 1.5px rgba(0, 0, 0, 0.25);
+    font-weight: 700;
+    font-size: 1.2rem;
+    text-align: center;
+    margin: 1rem 0;
+    cursor: pointer;
+    transition: all ease-in-out 0.6s;
+    :hover {
+      transform: scale(1.1);
+      color: #f4f5f9;
+    }
+  `}
+`;
