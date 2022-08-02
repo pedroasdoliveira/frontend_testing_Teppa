6b51a7cfd022adc41476624cbddc06780e67f8e4
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 18:43:31 -0300

    feat(theme): implement constants and mixins

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,7 +1,11 @@
 import { colors } from './colors'
+import { constants } from './constant'
+import { mixins } from './mixins'
 
 const theme = {
     colors,
+    constants,
+    mixins,
 } as const
 
 export default theme
