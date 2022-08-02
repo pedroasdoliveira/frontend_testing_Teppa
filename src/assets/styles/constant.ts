9805bafc4e863cf78cdd6a6db502b33c6ef56757
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 18:42:45 -0300

    feat(constants): implement constants

---
+++ src/assets/styles/constant.ts
@@ -0,0 +1,10 @@
+export const constants = {
+    titleFont: 'Roboto, sans-serif',
+    titleFontSize: '2rem',
+    titleFontWeight: '500',
+
+    bodyFont: 'Roboto, sans-serif',
+    bodyFontSize: '1rem',
+    bodyFontWeight: '300',
+    bodyLine: '140%'
+}
