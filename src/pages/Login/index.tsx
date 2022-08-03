1a51a689aaab7aaa57a6d78acc21b9796e1e7260
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-03 18:12:32 -0300

    feat(login): implement modifications

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,38 @@
-import * as Style from './style'
+import { useNavigate } from "react-router-dom";
+import Card from "../../components/Cards";
+import * as Style from "./style";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <div>Login</div>
-  )
-}
+    <Style.MainPage>
+      <Card>
+        <Style.LoginForm>
+          <Style.LoginInput
+            type="text"
+            name="email"
+            id="email"
+            placeholder=" Digite o E-mail de acesso"
+          />
 
-export default Login
+          <Style.LoginInput
+            type="text"
+            name="password"
+            id="password"
+            placeholder=" Digite a senha de acesso"
+          />
+
+          <Style.RegisterUser>
+            Não possui cadastro?{" "}
+            <strong onClick={() => navigate("/register")}>
+              Cadastre-se aqui
+            </strong>
+          </Style.RegisterUser>
+
+        </Style.LoginForm>
+      </Card>
+    </Style.MainPage>
+  );
+};
+
+export default Login;
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,2 +1,50 @@
 import styled, { css } from "styled-components";
 
+export const MainPage = styled.main`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primaryColor};
+    min-width: 100vw;
+    min-height: 100vh;
+    text-align: center;
+    ${theme.mixins.bodyStyle}
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const LoginForm = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const LoginInput = styled.input`
+  ${({ theme }) => css`
+    ${theme.mixins.bodyStyle}
+    width: 395px;
+    height: 40px;
+    border-radius: 15px;
+    background-color: ${theme.colors.bodyColorInput};
+    color: #000;
+    border: 1px solid #000;
+    outline: none;
+    margin-bottom: 1rem;
+  `}
+`;
+export const RegisterUser = styled.p`
+  ${({ theme }) => css`
+    ${theme.mixins.bodyStyle}
+    padding: 1rem;
+    strong {
+      cursor: pointer;
+      color: ${theme.colors.primaryColor};
+      :hover {
+        text-decoration: underline;
+      }
+    }
+  `}
+`;
