c93153c07c241b4c07f0929db073aa90a535f32d
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-04 16:04:28 -0300

    feat(Step01): implement the component and styles

--- src/components/Steps/step_1/index.tsx
+++ src/components/Steps/step_1/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import * as Style from "./style";
 
 const PersonalInformations = () => {
   return (
-    <div>PersonalInformations</div>
-  )
-}
+    <Style.Form>
+      <Style.InfoForm>Informações do usuário</Style.InfoForm>
 
-export default PersonalInformations
+      <Style.Input
+        type="text"
+        placeholder=" Digite o seu nome"
+        name="name"
+        id="name"
+      />
+
+      <Style.Input
+        type="email"
+        placeholder=" Digite o seu E-mail"
+        name="email"
+        id="email"
+      />
+
+      <Style.Input
+        type="number"
+        placeholder=" Digite a sua idade"
+        name="age"
+        id="age"
+      />
+      
+    </Style.Form>
+  );
+};
+
+export default PersonalInformations;
--- src/components/Steps/step_1/style.ts
+++ src/components/Steps/step_1/style.ts
@@ -1,2 +1,34 @@
 import styled, { css } from "styled-components";
 
+export const Form = styled.form`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `}
+`;
+
+export const InfoForm = styled.h2`
+    ${({theme}) => css`
+        ${theme.mixins.titleStyle}
+        font-size: 1.5rem;
+        text-align: center;
+        margin: 1rem 0;
+    `}
+`
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    ${theme.mixins.bodyStyle}
+    width: 395px;
+    height: 40px;
+    border-radius: 15px;
+    background-color: ${theme.colors.bodyColorInput};
+    color: #000;
+    border: 1px solid #000;
+    outline: none;
+    margin-bottom: 1rem;
+    padding: 0 .5rem;
+  `}
+`;
