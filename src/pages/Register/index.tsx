fa5c238b2623b2860e77eef3267da0db4d1afae5
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-04 16:07:12 -0300

    feat(RegisterUser): import MultiStep component

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,14 @@
 import Card from '../../components/Cards'
+import MultiStep from '../../components/Steps/multiStep'
 import * as Style from './style'
 
 const RegisterUser = () => {
   return (
     <Style.MainPage>
       <Card>
-        
+        <div>
+          <MultiStep />
+        </div>
       </Card>
     </Style.MainPage>
   )
