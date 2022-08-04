8ef01ad908f813c884dcfe4cb79497bbd6aacbd1
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-04 16:03:44 -0300

    feat(multiStep): implement component and style

---
+++ src/components/Steps/multiStep/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PersonalInformations from "../step_1";
+
+const MultiStep: any = () => {
+  const steps = [
+    {
+      name: "Informações",
+      component: <PersonalInformations />,
+    },
+  ];
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default MultiStep;
